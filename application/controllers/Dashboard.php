768502032675cb9f90cc07a0ff2725e96b9e8d8d
ceptarityas <ceptarityas21@gmail.com>
2021-04-09 23:00:57 +0700

    dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -46,7 +46,7 @@ class Dashboard extends CI_Controller
     }
     public function akunpengguna()
     {
-        $data['title'] = 'Gadai- Akun';
+        $data['title'] = 'Gadai-Akun';
         $data['user'] = $this->db->get_where('user_admin', ['email' => $this->session->userdata('email')])->row_array();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/topbarlogo', $data);

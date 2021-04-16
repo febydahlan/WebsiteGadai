be8cadc9d7d38d09a322e61149120ce4e849beb5
febydahlan <febbydahlan034@gmail.com>
2021-04-16 10:38:59 +0800

    PengajuanApi

---
+++ application/controllers/PengajuanApi.php
@@ -0,0 +1,46 @@
+<?php
+
+use chriskacerguis\RestServer\RestController;
+
+class PengajuanApi extends RestController
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Pengajuan_model');
+    }
+
+    public function index_get()
+    {
+        $pengajuan = $this->Pengajuan_model->getDataPengajuan();
+
+        if ($pengajuan) {
+            $this->response([
+                'status' => true,
+                'data' => $pengajuan
+            ], 200);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Data Tidak Ditemukan'
+            ], 404);
+        }
+    }
+
+    public function riwayat_get()
+    {
+        $pengajuan_riwayat = $this->Pengajuan_model->getDataRiwayat();
+
+        if ($pengajuan_riwayat) {
+            $this->response([
+                'status' => true,
+                'data' => $pengajuan_riwayat
+            ], 200);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'Data Tidak Ditemukan'
+            ], 404);
+        }
+    }
+}
